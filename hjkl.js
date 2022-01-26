199e291330942b855b93e8a28e124f8f32e9ae9f
mec-nyan <marianoraspberrypi@gmail.com>
2022-01-25 21:26:45 -0300

    feat(visual): Improved vim motion * use hjkl to move a 'container' selector * use count prefix (2j == move 2 blocks down)

--- hjkl.js
+++ hjkl.js
@@ -8,35 +8,60 @@ root.appendChild(big);
 const rects = [[], [], [], [], [], [], [], []];
 
 for (let i = 0; i < 8; ++i) {
-  for (let j = 0; j < 4; ++j) {
+  for (let j = 0; j < 5; ++j) {
     const rect = document.createElement("div");
     rect.setAttribute("class", "rect");
+    const img = document.createElement("img");
+    img.src = "./img/debian.png";
+    img.alt = "";
+    rect.appendChild(img);
     rects[i].push(rect);
     big.appendChild(rect);
   }
 }
 
+const selector = document.createElement("div");
+selector.setAttribute("id", "selector");
+big.appendChild(selector);
+
 let row = 0;
 let col = 0;
 rects[row][col].classList.add("selected");
+
+let prefix = 1;
+
 const select = (key) => {
   switch (key) {
     case "j":
-      row < 7 && ++row;
+      row = row + prefix <= 7 ? row + prefix : 7;
+      prefix = 1;
       break;
     case "k":
-      row > 0 && --row;
+      row = row - prefix >= 0 ? row - prefix : 0;
+      prefix = 1;
       break;
     case "h":
-      col > 0 && --col;
+      col = col - prefix >= 0 ? col - prefix : 0;
+      prefix = 1;
       break;
     case "l":
-      col < 3 && ++col;
+      col = col + prefix <= 4 ? col + prefix : 4;
+      prefix = 1;
+      break;
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      prefix = parseInt(key);
       break;
     default:
       break;
   }
-  for (let c = 0; c < 4; ++c) {
+  for (let c = 0; c < 5; ++c) {
     for (let r = 0; r < 8; ++r) {
       if (c === col && r === row) {
         rects[r][c].classList.add("selected");
@@ -45,6 +70,8 @@ const select = (key) => {
       }
     }
   }
+  selector.style.top = `${row * 12.5}%`;
+  selector.style.left = `${col * 20}%`;
 };
 
 document.body.onkeydown = (e) => select(e.key);
